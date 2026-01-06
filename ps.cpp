430cc54da71bec132ff3162eb51092bfdcc82c6e
holelack <holelack.py@gmail.com>
2026-01-06 15:13:15 +0900

    Add files via upload

---
+++ ps.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define fastio() {ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);}
+using namespace std;
+
+void init() {
+    fastio();
+    
+}
+
+int main() {
+    init();
+    
+
+    return 0;
+}
