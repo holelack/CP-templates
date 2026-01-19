c997ab608b79f5eed902f7069cecec7c18e90e80
holelack <holelack.py@gmail.com>
2026-01-20 00:24:17 +0900

    Refactor ps.cpp for improved readability

--- ps.cpp
+++ ps.cpp
@@ -1,8 +1,8 @@
 // standard template of mine
 #include <bits/stdc++.h>
-#define fastio() {ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);}
 using namespace std;
-
+using ll = long long int;
+#define fastio() {ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);}
 void init() {
     fastio();
     
@@ -15,3 +15,4 @@ int main() {
     return 0;
 
 }
+
