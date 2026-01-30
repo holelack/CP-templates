2deacfcb68fb830aac4fb4048af698f1c756429a
holelack <holelack.py@gmail.com>
2026-01-30 22:25:10 +0900

    Enhance ps.cpp with debugging utilities

--- ps.cpp
+++ ps.cpp
@@ -2,7 +2,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long int;
+#define minpq(T) priority_queue<T, vector<T>, greater<T>>
 #define fastio() {ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);}
+template<typename T>
+void debug(const T& x, const std::string& name) {
+    std::cout << name << ": " << x << '\n';
+}
+#define dbg(x) debug((x), #x)
+
 void init() {
     fastio();
     
@@ -15,4 +22,5 @@ int main() {
     return 0;
 
 }
+
 
