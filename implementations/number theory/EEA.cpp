36cd55be591059fd0c8cad9dc8ffdeda6074e6fb
holelack <holelack.py@gmail.com>
2026-07-03 01:08:02 +0900

    Implemented Extended Euclidean Algorithm

--- implementations/number theory/EEA.cpp
+++ implementations/number theory/EEA.cpp
@@ -1,1 +1,21 @@
+pii EEA(int a, int b) {
+    vector<int> r[3];
+    r[0] = {a, b}:
+    r[1] = {1, 0};
+    r[2] = {0, 1};
+    while (r[0].back()) {
+        int len = r[0].size();
+        int q = r[0][len-2] / r[0].back();
+        for (int i = 0; i < 3; i++) {
+            r[i].push_back(r[i][len-2] - q * r[i].back());
+        }
+    }
+    int len = r[0].size();
+    int s = r[1][len-2], t = r[2][len-2];
+    if (s < 0) {
+        s += b;
+        t -= a;
+    }
 
+    return {s,t};
+}
