ae3d3c7d90e36c8e206553da2e859ba19694016d
holelack <holelack.py@gmail.com>
2026-07-10 21:09:20 +0900

    Refactor LCA implementation for efficiency and clarity

--- implementations/tree/lca.cpp
+++ implementations/tree/lca.cpp
@@ -1,79 +1,70 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
+using pll = pair<ll, ll>;
 #define fastio() {ios::sync_with_stdio(0); cin.tie(0);}
-const int MAXN = 1e5 + 1, MAXD = 19;
-int n, m;
-int parent[MAXN][MAXD], dep[MAXN];
-vector<int> adj[MAXN];
+const ll MAXN = 2e5 + 1, LOG = 20;
+int N;
+struct Edge {
+    ll v, w;
+    Edge() {}
+    Edge(ll v, ll w) : v(v), w(w) {}
+};
+vector<Edge> G[MAXN];
+
+ll lev[MAXN];
+ll P[MAXN][LOG], W[MAXN][LOG];
 
 void dfs(int u, int p) {
-	parent[u][0] = p;
-	for (int v : adj[u]) {
-		if (v == p) continue;
-		dep[v] = dep[u] + 1;
-		dfs(v, u);
-	}
+    P[u][0] = p;
+    for (auto [v, w] : G[u]) {
+        if (v == p) continue;
+        W[v][0] = w;
+        lev[v] = lev[u] + 1;
+        dfs(v, u);
+    }
 }
 
-void getTable() {
-	for (int depth = 1; depth < MAXD; depth++) {
-		for (int u = 1; u <= n; u++) {
-			parent[u][depth] = parent[parent[u][depth - 1]][depth - 1];
-		}
-	}
+void build_lca() {
+    memset(W[0], 0, sizeof(W[0]));
+    memset(W[1], 0, sizeof(W[1]));
+    memset(P[0], 0, sizeof(P[0]));
+    memset(P[1], 0, sizeof(P[1]));
+    lev[0] = 0; lev[1] = 1;
+    dfs(1, 0);
+
+    for (int dep = 1; dep < LOG; dep++) {
+        for (int u = 1; u <= N; u++) {
+            P[u][dep] = P[P[u][dep - 1]][dep - 1];
+            W[u][dep] = W[u][dep - 1] + W[P[u][dep - 1]][dep - 1];
+        }
+    }
 }
 
-void init() {
-	fastio();
-	cin >> n;
-	int a, b;
-	for (int i = 1; i < n; i++) {
-		cin >> a >> b;
-		adj[a].push_back(b);
-		adj[b].push_back(a);
-	}
-	memset(dep, 0, sizeof(dep));
-	dfs(1, 0);
-	getTable();
-}
+pll dist(int a, int b) { // return pll(dist, LCA);
+    if (lev[a] > lev[b]) swap(a, b);
 
+    ll D = 0;
+    for (int dep = LOG - 1; dep >= 0; dep--) {
+        if (lev[b] - lev[a] >= (1 << dep)) {
+            D += W[b][dep];
+            b = P[b][dep];
+        }
+    }
 
-int lca(int a, int b) {
-	if (dep[a] < dep[b]) swap(a, b);
+    if (a == b) {
+        return pll(D, a);
+    }
 
-	for (int depth = MAXD - 1; depth >= 0; depth--) {
-		if (!parent[a][depth]) continue;
-		if (dep[a] - (1 << depth) >= dep[b]) {
-			a = parent[a][depth];
-		}
-	}
+    for (int dep = LOG - 1; dep >= 0; dep--) {
+        if (P[a][dep] != P[b][dep]) {
+            D += W[a][dep] + W[b][dep];
+            a = P[a][dep];
+            b = P[b][dep];
+        }
+    }
 
-	if (a == b) return a;
-
-	for (int depth = MAXD - 1; depth >= 0; depth--) {
-		if (!parent[a][depth]) continue;
-		if (parent[a][depth] != parent[b][depth]) {
-			a = parent[a][depth];
-			b = parent[b][depth];
-		}
-	}
-
-	return parent[a][0];
-}
-
-void query() {
-	cin >> m;
-	int a, b;
-	for (int i = 0; i < m; i++) {
-		cin >> a >> b;
-		cout << lca(a, b) << '\n';
-	}
-}
-
-int main() {
-	init();
-	query();
-
-	return 0;
+    D += W[a][0] + W[b][0];
+    a = P[a][0];
+    return pll(D, a);
 }
