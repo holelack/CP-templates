b1807409eeb520eb260af22c671e01a51764ca6b
holelack <holelack.py@gmail.com>
2026-07-10 21:05:14 +0900

    Refactor ps.cpp for debugging and cleanup

--- ps.cpp
+++ ps.cpp
@@ -1,14 +1,12 @@
-// standard template of mine
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << "[DEBUG] " << #x << " = " << x << '\n'
+#else
+#define debug(x)
+#endif
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long int;
-#define minpq(T) priority_queue<T, vector<T>, greater<T>>
+using ll = long long;
 #define fastio() {ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);}
-template<typename T>
-void debug(const T& x, const std::string& name) {
-    std::cout << name << ": " << x << '\n';
-}
-#define dbg(x) debug((x), #x)
 
 void init() {
     fastio();
@@ -18,9 +16,7 @@ void init() {
 int main() {
     init();
     
-
     return 0;
-
 }
 
 
