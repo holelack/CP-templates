2ea83f44e72bdaa426783ae3f3b4f2b8d99cce3f
holelack <holelack.py@gmail.com>
2026-07-16 21:39:11 +0900

    Refactor 2SAT implementation for clarity and efficiency

--- implementations/graphs/2sat.cpp
+++ implementations/graphs/2sat.cpp
@@ -2,59 +2,37 @@
 using namespace std;
 using ll = long long;
 #define fastio() {ios::sync_with_stdio(0); cin.tie(0);}
-const int MAXN = 1e6 + 1, MAXM = 1e6 + 1;
+const int MAXN = 1e6 + 1;
 int N, M;
+vector<int> G[MAXN << 1];
 
-vector<int> adj[MAXN<<1];
-
-vector<vector<int>> SCC;
-int low[MAXN << 1], disc[MAXN << 1], visited[MAXN << 1], ID[MAXN << 1];
+int low[MAXN << 1], disc[MAXN << 1], visited[MAXN << 1], ID[MAXN<<1];
 stack<int> stk;
 int dfsn = 0, sccid = 0;
 
-void dfs(int u) {
+int dfs(int u) {
+	disc[u] = low[u] = ++dfsn;
 	visited[u] = 1;
 	stk.push(u);
-	disc[u] = low[u] = ++dfsn;
-
-	for (int v : adj[u]) {
-		if (!disc[v]) {
-			dfs(v);
-			low[u] = min(low[u], low[v]);
-		}
+	for (int v : G[u]) {
+		if (!disc[v]) low[u] = min(low[u], dfs(v));
 		else if (visited[v]) low[u] = min(low[u], disc[v]);
 	}
-
 	if (low[u] == disc[u]) {
-		SCC.emplace_back();
 		sccid++;
-		while (!stk.empty()) {
-			int v = stk.top(); stk.pop();
-			visited[v] = false;
-			SCC.back().push_back(v);
+		int v;
+		do {
+			v = stk.top(); stk.pop();
+			visited[v] = 0;
 			ID[v] = sccid;
-			if (u == v) break;
-		}
+		} while (u != v);
 	}
+	return low[u];
 }
 
-void addClause(int u, int v) {
-	adj[u ^ 1].push_back(v);
-	adj[v ^ 1].push_back(u);
-}
-
-bool SAT[MAXN];
-
-bool solveSAT() {
-	for (int i = 1; i <= N; i++) {
-		if (ID[i<<1] == ID[i<<1|1]) return false;
-	}
-
-	for (int i = 1; i <= N; i++) {
-		SAT[i] = ID[i<1] < ID[i<<1|1] ? true : false;
-	}
-
-	return true;
+void addEdge(int u, int v) {
+	G[u ^ 1].push_back(v);
+	G[v ^ 1].push_back(u);
 }
 
 void init() {
@@ -62,21 +40,22 @@ void init() {
 	cin >> N >> M;
 	int u, v;
 	for (int i = 0; i < M; i++) {
-		cin >> u >> v;
-		addClause(u<<1, v<<1);
+		cin >> u >> v; u--; v--;
+		addEdge(u << 1, v << 1);
 	}
 
-	memset(low, 0, sizeof(low));
-	memset(disc, 0, sizeof(disc));
-	memset(visited, 0, sizeof(visited));
+	for (int i = 0; i < (N << 1); i++) {
+		if (!disc[i]) dfs(i);
+	}
 }
 
+bool sat[MAXN];
 int main() {
 	init();
-	for (int u = 2; u <= (N<<1|1); u++) {
-		if (!disc[u]) dfs(u);
+	
+	for (int i = 0; i < N; i++) {
+		sat[i] = ID[i << 1] < ID[i << 1 | 1];
 	}
-  solveSAT();
 
 	return 0;
 }
