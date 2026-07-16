be9cf522080491e944f300077ed6c29c828d407b
holelack <holelack.py@gmail.com>
2026-07-17 08:33:21 +0900

    Implement articulation point algorithm in art_point.cpp

---
+++ implementations/graphs/BCC/art_point.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define fastio() {ios::sync_with_stdio(0); cin.tie(0);}
+const int MAXN = 1e6 + 1;
+int N, M;
+vector<int> G[MAXN];
+
+int disc[MAXN], low[MAXN], cut[MAXN];
+int dfsn = 0;
+int dfs(int u, int p = 0) {
+	disc[u] = low[u] = ++dfsn;
+	int child = 0;
+	for (int v : G[u]) {
+		if (disc[v]) low[u] = min(low[u], disc[v]); // back edge
+		else {
+			low[u] = min(low[u], dfs(v,u)); // tree edge
+			if (disc[u] <= low[v] && p) cut[u] = 1; // 단절점 조건
+			child++;
+		}
+	}
+	if (p == 0 && child >= 2) cut[u] = 1; // root exception
+	return low[u];
+}
+
+void init() {
+	fastio();
+	cin >> N >> M;
+	int u, v;
+	for (int i = 0; i < M; i++) {
+		cin >> u >> v;
+		G[u].push_back(v);
+		G[v].push_back(u);
+	}
+}
+
+int main() {
+	init();
+	for (int u = 1; u <= N; u++) if (!disc[u]) dfs(u);
+	for (int u = 1; u <= N; u++) if (cut[u]) cout << u << " ";
+
+	return 0;
+}
