84a9424eecb31153ec2b552c649e9047b47e3903
holelack <holelack.py@gmail.com>
2026-08-02 18:23:30 +0900

    Refactor DST structure and update methods

--- implementations/range queries/DST.cpp
+++ implementations/range queries/DST.cpp
@@ -3,45 +3,55 @@ using namespace std;
 using ll = long long;
 #define fastio() {ios::sync_with_stdio(0); cin.tie(0);}
 const int MAXN = 1e9;
-int N;
 
 struct Node {
-	int l = 0, r = 0, val = 0;
-	Node() {}
+	int l = 0, r = 0;
+	ll v = 0;
+	Node() = default;
+	Node(ll v) : v(v) {}
+	Node(int l, int r, ll v) : l(l), r(r), v(v) {}
+	Node operator+(const Node& o) const {
+		return Node(l, r, v + o.v);
+	}
 };
 
-vector<Node> tree = { Node() };
+template <typename Node>
 struct DST {
-  int root = 0;
+private:
+	vector<Node> tree;
 	int alloc() {
 		tree.push_back(Node());
 		return tree.size() - 1;
 	}
 
-	int update(int node, int i, int v, int s = 1, int e = N) {
+	int update(int i, Node v, int s = 1, int e = MAXN, int node = 1) {
 		if (node == 0) node = alloc();
-		tree[node].val += v;
+		tree[node] = tree[node] + v;
 		if (s == e) return node;
 		int mid = s + e >> 1;
-		if (i <= mid) tree[node].l = update(tree[node].l, i, v, s, mid);
-		else tree[node].r = update(tree[node].r, i, v, mid + 1, e);
+		if (i <= mid) tree[node].l = update(i, v, s, mid, tree[node].l);
+		else tree[node].r = update(i, v, mid + 1, e, tree[node].r);
 		return node;
 	}
 
-	int query(int node, int l, int r, int s = 1, int e = N) {
-		if (node == 0 || r < s || e < l) return 0;
-		if (l <= s && e <= r) return tree[node].val;
+	Node query(int l, int r, int s = 1, int e = MAXN, int node = 1) {
+		if (node == 0 || r < s || e < l) return Node();
+		if (l <= s && e <= r) return tree[node];
 		int mid = s + e >> 1;
-		return query(tree[node].l, l, r, s, mid) + query(tree[node].r, l, r, mid + 1, e);
+		return query(l, r, s, mid, tree[node].l) + query(l, r, mid + 1, e, tree[node].r);
 	}
+public:
+	DST() {
+		tree.clear();
+		tree.push_back(Node());
+		tree.push_back(Node());
+	}
+	void upd(int i, Node v) { update(i, v); };
+	Node qry(int l, int r) { return query(l, r); }
 };
-DST dst;
-
-void init() {
-  root = update(0,1,1);
-}
 
 int main() {
-  init();
-  return 0;
+	DST<Node> dst = DST<Node>();
+
+	return 0;
 }
