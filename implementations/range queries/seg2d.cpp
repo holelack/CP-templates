2bda4216c1957eedbc290e86f986d398750aed8d
holelack <holelack.py@gmail.com>
2026-08-03 12:00:17 +0900

    Refactor seg2D class and update query methods

--- implementations/range queries/seg2d.cpp
+++ implementations/range queries/seg2d.cpp
@@ -1,71 +1,65 @@
-#include <bits/stdc++.h>
-using namespace std;
-using ll = long long;
-#define fastio() {ios::sync_with_stdio(0); cin.tie(0);}
+
 struct Node {
-	int l = 0, r = 0, val = 0;
+	int l = 0, r = 0;
+	int v = 0;
 	Node() {}
+	Node(int v) : v(v) {}
+	Node(int l, int r, int v) : l(l), r(r), v(v) {}
+	Node operator+(const Node& o) const {
+		return Node(l, r, v + o.v);
+	}
 };
 
-vector<Node> tree = { Node() };
+template <typename Node>
+class DST {
+private:
+	vector<Node> tree;
+	int alloc() {
+		tree.push_back(Node());
+		return tree.size() - 1;
+	}
+public:
+	DST() {
+		tree.clear();
+		tree.push_back(Node());
+		tree.push_back(Node());
+	}
+	int upd(int i, Node v, int s = 1, int e = MAXN, int node = 1) {
+		if (node == 0) node = alloc();
+		tree[node] = tree[node] + v;
+		if (s == e) return node;
+		int mid = s + e >> 1;
+		if (i <= mid) tree[node].l = upd(i, v, s, mid, tree[node].l);
+		else tree[node].r = upd(i, v, mid + 1, e, tree[node].r);
+		return node;
+	}
+
+	Node qry(int l, int r, int s = 1, int e = MAXN, int node = 1) {
+		if (node == 0 || r < s || e < l) return Node();
+		if (l <= s && e <= r) return tree[node];
+		int mid = s + e >> 1;
+		return qry(l, r, s, mid, tree[node].l) + qry(l, r, mid + 1, e, tree[node].r);
+	}
+};
+
 class seg2D {
 private:
-	int root[MAXN];
-	struct DST {
-		int alloc() {
-			tree.push_back(Node());
-			return tree.size() - 1;
-		}
-
-		int update(int node, int i, int v, int s = 1, int e = N) {
-			if (node == 0) node = alloc();
-			tree[node].val += v;
-			if (s == e) return node;
-			int mid = s + e >> 1;
-			if (i <= mid) tree[node].l = update(tree[node].l, i, v, s, mid);
-			else tree[node].r = update(tree[node].r, i, v, mid + 1, e);
-			return node;
-		}
-
-		int query(int node, int l, int r, int s = 1, int e = N) {
-			if (node == 0 || r < s || e < l) return 0;
-			if (l <= s && e <= r) return tree[node].val;
-			int mid = s + e >> 1;
-			return query(tree[node].l, l, r, s, mid) + query(tree[node].r, l, r, mid + 1, e);
-		}
-	};
-	DST dst;
-
-	void update(int x, int y, int v) {
-		for (int i = x; i <= N; i += i & -i) {
-			root[i] = dst.update(root[i], y, v);
+	DST<Node> BIT[MAXN];
+public:
+	void upd(int x, int y, int v) {
+		for (int i = x; i < MAXN; i += i & -i) {
+			BIT[i].upd(y, Node(v));
 		}
 	}
 
-	int query(int x, int y1, int y2) {
+	int qry(int x1, int x2, int y1, int y2) {
 		int res = 0;
-		for (int i = x; i > 0; i -= i & -i) {
-			res += dst.query(root[i], y1, y2);
+		for (int i = x2; i > 0; i -= i & -i) {
+			res = res + BIT[i].qry(y1, y2).v;
+		}
+		for (int i = x1-1; i > 0; i -= i & -i) {
+			res = res - BIT[i].qry(y1, y2).v;
 		}
 		return res;
 	}
-
-public:
-	void build() {
-		tree.clear();
-		tree.push_back(Node());
-		memset(root, 0, sizeof(root));
-	}
-	void PsumU(int x, int y, int v) { update(x, y, v); }
-	int RsumQ(int x1, int x2, int y1, int y2) { return query(x2, y1, y2) - query(x1-1, y1, y2); }
 };
-
-void init() {
-  seg2D.build();
-}
-
-int main() {
-  init();
-
-  return 0;
-}
